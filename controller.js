ece1884e8aa5ab533524e3b5ecfbb29cc541cb43
Nathaniel Collins <nathancleon@gmail.com>
2018-08-03 11:02:57 -0400

    updated database url

--- controller.js
+++ controller.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user:pass123@ds137370.mlab.com:37370/blog-app-mongoose';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app-mongoose';
 exports.PORT = process.env.PORT || 8080;
